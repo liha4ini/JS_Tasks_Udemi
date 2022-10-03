204af849a6987ef0e9ec20d0fcc9b078394f3eaf
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-03 21:26:29 +0300

    loops_8

---
+++ loops_8.js
@@ -0,0 +1,13 @@
+// Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i]
+    }
+    return result;
+}
