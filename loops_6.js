50af5ca7c876e01d4a50c8e60f5d155de0775b06
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-03 21:22:56 +0300

    loops_6

---
+++ loops_6.js
@@ -0,0 +1,12 @@
+// Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+
+
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    return result;
+}
