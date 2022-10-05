6179076707b2d0b5dc418c528329b9b2c37d47d5
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-05 18:47:33 +0300

    obj_2

---
+++ obj_2.js
@@ -0,0 +1,22 @@
+// Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `
+    });
+
+    return str;
+}
+
+showFamily(family);
