077d867964dc2d5f2920a51077482179ef9bee0f
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-12 19:56:11 +0300

    array_methods_2

---
+++ array_methods_2.js
@@ -0,0 +1,33 @@
+// Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+
+// Пример:
+
+// showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
+
+
+
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showListOfFilms(arr) {
+    return arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
+}
+
+showListOfFilms(films);
