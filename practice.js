624dd6c9b0e8df75dd869ade9dabdb000340e5d2
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-25 17:40:07 +0300

    added if

--- practice.js
+++ practice.js
@@ -48,10 +48,15 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = +prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
 }
 
 console.log(personalMovieDB);
