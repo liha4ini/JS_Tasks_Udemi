18367773cbb1075c3191b1dc72e6492208bc89e8
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-26 17:54:04 +0300

    removeDupes_1

---
+++ removeDupes.js
@@ -0,0 +1,22 @@
+// ### Удаление всех повторяющихся значений в строке
+
+// Напишите функцию, которая принимает строку и возвращает новую, в которой все дублирующиеся символы будут удалены.
+
+// **Input**: String
+
+// **Output**: String
+
+
+function removeDupes(str) {
+    const chars = {}
+    const res = []
+  
+    for (let i = 0; i < str.length; i++) {
+      if (!chars[str[i]]) {
+        chars[str[i]] = true
+        res.push(str[i])
+      }
+    }
+  
+    return res.join('')
+  }
