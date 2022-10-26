e3764bb5ca37bd5214ab1c9fd5634232d0095004
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-26 17:55:15 +0300

    removeDupes_2

--- removeDupes.js
+++ removeDupes.js
@@ -8,15 +8,17 @@
 
 
 function removeDupes(str) {
-    const chars = {}
-    const res = []
+    // const chars = {}
+    // const res = []
   
-    for (let i = 0; i < str.length; i++) {
-      if (!chars[str[i]]) {
-        chars[str[i]] = true
-        res.push(str[i])
-      }
-    }
+    // for (let i = 0; i < str.length; i++) {
+    //   if (!chars[str[i]]) {
+    //     chars[str[i]] = true
+    //     res.push(str[i])
+    //   }
+    // }
   
-    return res.join('')
+    // return res.join('')
+
+    return Array.from(new Set(str)).join('')
   }
