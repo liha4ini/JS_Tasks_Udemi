3c8021be963af0d3dd8852890270b3cdb0487440
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-27 09:13:23 +0300

    isStringRotated_2

--- isStringRotated.js
+++ isStringRotated.js
@@ -8,19 +8,20 @@
 
 
 function isStringRotated(source, test) {
-    if (source.length !== test.length) {
-        return false
-    }
+    // if (source.length !== test.length) {
+    //     return false
+    // }
     
-    for (let i = 0; i < source.length; i++) {
-        const rotate = source.slice(i, source.length) + source.slice(0, i)
+    // for (let i = 0; i < source.length; i++) {
+    //     const rotate = source.slice(i, source.length) + source.slice(0, i)
     
-        if (rotate === test) {
-            return true
-        }
-    }
+    //     if (rotate === test) {
+    //         return true
+    //     }
+    // }
     
-    return false
+    // return false
+    return source.length === test.length && (source + source).includes(test)
 }
 
 console.log(isStringRotated('javascript', 'scriptjava')) // -> true
