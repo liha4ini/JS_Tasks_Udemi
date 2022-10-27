7adcabb876a94749e90f4ef27f75ae7a69a4590d
Sergey Lihachev <liha4ev.sergey@gmail.com>
2022-10-27 09:12:40 +0300

    isStringRotated

---
+++ isStringRotated.js
@@ -0,0 +1,28 @@
+// ### Повернута ли строка?
+
+// Напишите функцию, которая принимает 2 строки. Верните `true` если строки являются перевернутым вариантом друг друга (см. пример). Иначе верните `false`.
+
+// **Input**: String, String
+
+// **Output**: Boolean
+
+
+function isStringRotated(source, test) {
+    if (source.length !== test.length) {
+        return false
+    }
+    
+    for (let i = 0; i < source.length; i++) {
+        const rotate = source.slice(i, source.length) + source.slice(0, i)
+    
+        if (rotate === test) {
+            return true
+        }
+    }
+    
+    return false
+}
+
+console.log(isStringRotated('javascript', 'scriptjava')) // -> true
+console.log(isStringRotated('javascript', 'iptjavascr')) // -> true
+console.log(isStringRotated('javascript', 'java')) // -> false
